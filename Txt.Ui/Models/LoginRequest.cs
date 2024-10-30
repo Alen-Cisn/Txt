b6d6757b8c6609ec0c1aef741883e148ed11849e
MrtSoria <martin_soria02@outlook.com>
2024-10-30 20:19:07 -0300

    Se reestructuro MainLayout y Home

--- Txt.Ui/Models/LoginRequest.cs
+++ Txt.Ui/Models/LoginRequest.cs
@@ -9,6 +9,7 @@ public sealed class LoginRequest
 {
     [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Invalid email")]
     public required string Email { get; set; }
+
     [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Incorrect password.")]
     public required string Password { get; set; }
     public string? TwoFactorCode { get; set; }
